1a90c4dffb1aa7198ee4edd16642648ad016e30b
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-09-05 15:52:25 -0600

    Latest version

--- js/added-scroll.js
+++ js/added-scroll.js
@@ -1,3 +1,10 @@
+// Adding scroll functionality for 'Learn More' button
 $('.learn-more').click(function () {
   $('html,body').animate({ scrollTop: $('#about').offset().top }, 1500);
 });
+
+// Adding scroll functionality so logo click brings back to top
+$('.logo').click(function () {
+  $('html,body').animate({ scrollTop: $('#home').offset().top }, 1500);
+});
+
