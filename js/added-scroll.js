ee652127ad7a3d43fe59c6274a1407e246d31506
Shadee Merhi <shadeemerhi@Shadees-MacBook-Pro-2.local>
2020-09-07 21:51:27 -0600

    Latest version

--- js/added-scroll.js
+++ js/added-scroll.js
@@ -3,6 +3,11 @@ $('.learn-more').click(function () {
   $('html,body').animate({ scrollTop: $('#about').offset().top }, 1500);
 });
 
+// Adding scroll functionality for the 'Reach Out' button in the footer
+$('.reach_out_btn').click(function () {
+  $('html,body').animate({ scrollTop: $('#contact').offset().top }, 2000);
+});
+
 // Adding scroll functionality so logo click brings back to top
 $('.logo').click(function () {
   $('html,body').animate({ scrollTop: $('#home').offset().top }, 1500);
